b478276bb622dfdaa273528e62ba2456fe136062
sahilsarmalkar <sahilgaming667@gmail.com>
2026-03-16 19:50:26 +0530

    modification done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 const app = require("./src/app");
-
 const connectDB = require("./src/config/db");
 
+const PORT = process.env.PORT || 3000;
 
+async function startServer() {
+  try {
+    await connectDB();   // wait for MongoDB
 
-connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
 
+  } catch (error) {
+    console.error("Server start failed:", error);
+  }
+}
 
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-});
+startServer();
